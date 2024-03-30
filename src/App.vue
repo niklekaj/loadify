afac65fb2b5eacc02beb0db99b786836c5caa207
Kristjan Niklekaj <dev.niklekaj@gmail.com>
2024-03-30 12:25:43 +0100

    feat: UI improvements

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,12 @@ import { RouterLink, RouterView } from 'vue-router'
 <style scoped lang="scss">
   nav {
     display: flex;
-    justify-content: space-between;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 16px;
+    margin-top: 16px;
+    gap: 24px;
+
   }
 
   .highlight {
